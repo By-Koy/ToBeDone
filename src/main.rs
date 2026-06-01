20b1798d072f4af75cd96247de2d81d5939daa43
By.Koy <koy@camerini.me>
2026-06-01 20:47:23 +0300

    Added basic deps and first tests

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,20 @@
-fn main() {
-    println!("Hello, world!");
+use std::io::stdout;
+use std::error::Error;
+
+use ratatui::prelude::Terminal;
+use ratatui::backend::CrosstermBackend;
+use crossterm::terminal::*;
+use crossterm::ExecutableCommand;
+
+fn main() -> Result<(), Box<dyn Error>> {
+    stdout().execute(EnterAlternateScreen)?;
+    enable_raw_mode()?;
+
+    let mut term = Terminal::new(CrosstermBackend::new(stdout()))?;
+    term.clear()?;
+    
+
+    stdout().execute(LeaveAlternateScreen)?;
+    disable_raw_mode()?;
+    Ok(())
 }
