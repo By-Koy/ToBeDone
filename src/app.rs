d3c86472963d9e12f48518254880027a385d4433
By.Koy <koy@camerini.me>
2026-06-05 13:49:02 +0300

    Moved the render function into the appState so that the terminal is only interfaced with through it

--- src/app.rs
+++ src/app.rs
@@ -9,26 +9,27 @@ pub struct State {
     counter: u8,
     exit: bool,
 } impl State {
-    pub fn run(&mut self, terminal: &mut DefaultTerminal) -> Result<(), Box<dyn Err>> {
+    pub fn run(&mut self, term: &mut DefaultTerminal) -> Result<(), Box<dyn Err>> {
+
+        term.draw(|frame| self.render(frame))?;
 
         while !self.exit {
-            main(self, terminal)?;
+            main(self)?;
         }
         Ok(())
     }
+
+    fn render(&self, frame: &mut Frame) {
+        frame.render_widget("self.counter", frame.area());
+    }
 } 
 
-pub fn main(app: &mut State, term: &mut DefaultTerminal) -> Result<(), Box<dyn Err>> {
+pub fn main(app: &mut State) -> Result<(), Box<dyn Err>> {
     let input = read()?;
-    
-    term.draw(render)?;
 
     if input.is_key_press() {
         app.exit = true;
     }
 
     Ok(())
-}
-fn render(frame: &mut Frame) {
-    frame.render_widget("hellodfgjdfhjfghj world", frame.area());
 }
