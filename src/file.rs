ba91d1ed35f425ab61e710f919ec73a853bfe152
By.Koy <koy@camerini.me>
2026-06-08 09:00:53 +0300

    Removed dev comments (woopsie)

--- src/file.rs
+++ src/file.rs
@@ -1,16 +1,8 @@
 use std::fs;
 use std::error::Error;
+use std::os::unix::fs as unix;
 
 use crate::app::State;
-
-// step 1. figure out note path from name DONE
-// step 1.b. if note doesn't exist, create it
-// step 1.c. if not given note name get last used
-// step 2. save path DONE
-// step 3. read and split file contents DONE
-
-// let contents = fs::read_to_string("/var/local/TBD/0*")?
-//                 .split("\n").map(|s| s.to_string()).collect();
 
 pub fn main(app: &mut State, args: Vec<String>) -> Result<(), Box<dyn Error>> {
     let id: &str;
@@ -21,18 +13,47 @@ pub fn main(app: &mut State, args: Vec<String>) -> Result<(), Box<dyn Error>> {
         id = &args[1];
     }
 
-    check_path(app, format!("/var/local/TBD/{id}.md"))?;
+    let check = check_path(app, &id);
+    if let Err(_) = check {
+        fs::write(format!("/var/local/TBD/{id}.md"), format!("**{id}**"))
+            .expect("unable to create/write to file, please check permissions.");
+
+        app.contents = vec!(format!("**{id}**"));
+        app.id = id.to_string();
+    }
+
     Ok(())
 }
 
-fn check_path(app: &mut State, path: String) -> Result<(), Box<dyn Error>> {
+fn check_path(app: &mut State, id: &str) -> Result<(), Box<dyn Error>> {
+    let path = format!("/var/local/TBD/{id}.md");
+    
     if let Err(e) = fs::File::open(&path) {
         return Err(Box::new(e));
     }
 
-    app.contents = fs::read_to_string(&path)?
+    app.contents = fs::read_to_string(path)?
                 .split("\n").map(|s| s.to_string()).collect();
-    app.file_path = path;
+    app.id = String::from(id);
 
     Ok(())
+}
+
+pub fn exit(app: &mut State) {
+    let path = format!("/var/local/TBD/{}.md", &app.id);
+    if app.contents.is_empty()  {
+        fs::remove_file(&path)
+            .expect("unable to remove note, please check permissions");
+    } else if app.id != "Recent" {
+        let rm = fs::remove_file("/var/local/TBD/Recent.md");
+        if let Err(_) = rm {
+            println!("unable to remove old symlink, please check permissions");
+        }
+
+        let symlink = unix::symlink(&path, "/var/local/TBD/Recent.md");
+
+        if let Err(_) = symlink {
+            println!("unable to create symlink, please check permissions");
+        }
+    }
 }
