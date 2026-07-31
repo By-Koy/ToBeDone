25408c129acc0ee9364e5e52ccf33319c3045628
By.Koy <koy@camerini.me>
2026-07-31 15:34:00 +0300

    Feat: added rudimentary command-line argument support

--- src/main.rs
+++ src/main.rs
@@ -3,10 +3,41 @@ use std::env;
 mod app;
 mod file;
 
+#[derive(Debug, Default)]
+struct Args {
+    debug: bool,
+    sample: bool,
+    no_eggs: bool,
+    other: Vec<String>
+} impl Args {
+
+    fn init(&mut self, input: Vec<String>) -> Vec<String> {
+        let args: Vec<String> = input.clone().into_iter().filter(|s| s.starts_with("--")).collect();
+        for arg in args {
+            match &*arg {
+                "--debug" => self.debug=true,
+                "--sample" => self.sample=true,
+                "--no-eggs" => self.no_eggs=true,
+                _ => self.other.push(arg)
+            };
+        }
+
+        if self.other.len() > 0 { println!("Unknown argument(s) {:?}.
+                                            \nPossible arguments are:
+                                            \n --debug - show debuging vars,
+                                            \n --sample - make sample.md with provided sample text,
+                                            \n --no-eggs - no eggs\n", self.other)}
+
+        input.into_iter().filter(|s| !s.starts_with("--")).collect()
+    }
+}
+
 fn main() {
-    let args: Vec<String> = env::args().collect();
+    let mut args: Args = Args::default();
+    let input: Vec<String>  = args.init(env::args().collect());
+
     let mut app = app::State::default();
 
-    file::main(&mut app, args);
+    file::main(&mut app, input);
     ratatui::run(|terminal| app.run(terminal)).unwrap();
 }
