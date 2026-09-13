c2a12a04599d83596fe9ad678725d17ef28a894e
By.Koy <koy@camerini.me>
2026-09-13 14:51:49 +0300

    Feat: Added uninstall.sh for one line uninstallation

---
+++ uninstall.sh
@@ -0,0 +1,20 @@
+#!/bin/zsh
+
+# Properly use sudo (requires sudo caching)
+# Source - https://stackoverflow.com/a/42876846
+# Posted by Socowi, modified by community. See post 'Timeline' for change history
+# *Further modified by Koy
+# Retrieved 2026-09-13, License - CC BY-SA 4.0
+if [[ "$EUID" = 0 ]]; then
+else
+    sudo -k # make sure to ask for password on next sudo ✱
+    if sudo true; then
+    else
+        echo "Script requires sudo"
+        exit 1
+    fi
+fi
+
+#Remove files
+rm -rf $XDG_DATA_HOME/TBD
+sudo rm -f /usr/local/bin/ToBeDone
