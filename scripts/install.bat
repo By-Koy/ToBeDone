fad1084321bdb4f50285d0eaac2fffcc73599d0f
Koy <koy@camerini.me>
2026-09-18 20:34:15 +0200

    Update install.bat

--- scripts/install.bat
+++ scripts/install.bat
@@ -2,9 +2,7 @@ SET TBD_NOTES_HOME=%LOCALAPPDATA%
 
 git clone https://github.com/By-Koy/ToBeDone.git
 CD ToBeDone
-cargo build --release
+cargo build --release && XCOPY target\release\ToBeDone.exe %PROGRAMFILES%\TBD\ && MD %LOCALAPPDATA%\TBD
 
-XCOPY target\release\ToBeDone.exe %PROGRAMFILES%\TBD\
-MD %LOCALAPPDATA%\TBD
 CD ..
 RMDIR ToBeDone
